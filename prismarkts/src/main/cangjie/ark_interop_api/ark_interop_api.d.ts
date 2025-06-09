ada7257f215255d9476cf2c79df60db3860855d4
peixianzhong <peixianzhong@noreply.gitcode.com>
2025-06-09 16:25:49 +0800

    ohos arkts

---
+++ prismarkts/src/main/cangjie/ark_interop_api/ark_interop_api.d.ts
@@ -0,0 +1,18 @@
+export declare class PrismNodesParseResColorArkts {
+    getBackground(): number
+    getDefaultFontColor(): number
+    getListColor(): Array<PrismNodesParseResArkts>
+}
+
+export declare class PrismNodesParseResArkts {
+    getType(): string
+    getText(): string
+    getAlias(): string | undefined
+    getColor(): number | undefined
+}
+
+export declare interface CustomLib {
+    PrismNodesParseResArkts: {new (types: string, text: string, alias: string | undefined, color: number | undefined): PrismNodesParseResArkts}
+    codeStringToColorStringArkts(code: string, info: string | undefined, isDarkula: boolean): PrismNodesParseResColorArkts
+    PrismNodesParseResColorArkts: {new (list: Array<PrismNodesParseResArkts>, background: number, defaultFontColor: number): PrismNodesParseResColorArkts}
+}
