415451b396079f1fea6430a00d789140e3545124
peixianzhong <peixianzhong@noreply.gitcode.com>
2025-07-07 10:40:24 +0800

    update 5.1.0

---
+++ prism/src/main/cangjie/types/libprism4cj/Index.d.ts
@@ -0,0 +1,19 @@
+export declare class PrismNodesParseResArkts {
+    getType(): string
+    getText(): string
+    getAlias(): string | undefined
+    getColor(): number | undefined
+    constructor (types: string, text: string, alias: string | undefined, color: number | undefined)
+}
+
+export declare class PrismNodesParseResColorArkts {
+    getBackground(): number
+    getDefaultFontColor(): number
+    getListColor(): Array<PrismNodesParseResArkts>
+    constructor (list: Array<PrismNodesParseResArkts>, background: number, defaultFontColor: number)
+}
+
+
+export declare function codeStringToColorStringArkts(code: string, info: string | undefined, isDarkula: boolean): PrismNodesParseResColorArkts
+
+export declare function codeStringToColorStringCustomizeArkts(code: string, info: string | undefined, background: number, text: number, colorMap: Map<string, number>): PrismNodesParseResColorArkts
