be18dc86b675bf65d2803c7d3fe70a1a53545f9d
yangtao <taoyangdd@isoftstone.com>
2025-08-06 20:31:47 +0800

    parse into js

--- prism/src/main/cangjie/ark_interop_api_prism/ark_interop_api_prism.d.ts
+++ prism/src/main/cangjie/ark_interop_api_prism/ark_interop_api_prism.d.ts
@@ -16,4 +16,21 @@ export declare interface CustomLib {
     PrismNodesParseResColorArkts: {new (list: Array<PrismNodesParseResArkts>, background: number, defaultFontColor: number): PrismNodesParseResColorArkts}
     codeStringToColorStringArkts(code: string, info: string | undefined, isDarkula: boolean): Promise<PrismNodesParseResColorArkts>
     codeStringToColorStringCustomizeArkts(code: string, info: string | undefined, background: number, text: number, colorMap: Map<string, number>): PrismNodesParseResColorArkts
+
+  /**
+   * parse result into js object
+   * @param code
+   * @param info
+   * @param isDarkula
+   * @param resColorFactory: (background: number,defaultFontColor: number)=>PrismNodesParseResColor
+   * @param resFactory: (types: string,text: string,alias: string | undefined,color: number | undefined)=>PrismNodesParseRes
+   * @returns Promise<PrismNodesParseResColor>
+   */
+  parseIntoJs(
+    code: string,
+    info: string | undefined,
+    isDarkula: boolean,
+    resColorFactory: (background: number, defaultFontColor: number) => object,
+    resFactory: (types: string, text: string, alias: string | undefined, color: number | undefined) => object
+  ): Promise<object>
 }
